68d32ac085ed3fd346afa1410722475e64e82419
marek9499 <32765296+marek9499@users.noreply.github.com>
2020-09-28 19:08:40 +0200

    fixed app launching port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const expbs = require('express-handlebars');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
 const routes = require('./routes/api')
-const port = process.env.port = 5501
+const port = (process.argv.slice(2).length == 1) ? process.argv.slice(2)[0] : 5000;
 const app = express()
 
 app.set('view engine', '.hbs');
@@ -18,17 +18,16 @@ app.use('/api', routes);
 app.use('/public', express.static('public'))
 
 
-app.get('/', function (req, res, next) {
-    res.render('home.hbs',
-        {
-            appPort: port
-        });
+app.get('/', function(req, res, next) {
+    res.render('home.hbs', {
+        appPort: port
+    });
 });
 
 app.get('*', (req, res) => {
     res.send("brak");
 });
 
-app.listen(process.env.port || 4000, () => {
-    console.log('listening for request is now live');
+app.listen(port, () => {
+    console.log(`listening for request is now live at port ${port}`);
 });
