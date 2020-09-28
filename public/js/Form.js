662fbf90c882ee0a285d1d7fa0ea4bf5a8ec5d4d
marek9499 <32765296+marek9499@users.noreply.github.com>
2020-09-28 20:12:38 +0200

    fixed bug when user added more than 4 cities

--- public/js/Form.js
+++ public/js/Form.js
@@ -1,26 +1,33 @@
 import * as Cookie from './Cookie.js';
 import * as Request from './Request.js';
 
-const lastSearchedCities = Cookie.get("lastSearchedCities") ? Cookie.get("lastSearchedCities").split(",") : undefined;
-console.log(lastSearchedCities);
+let lastSearchedCities = Cookie.get("lastSearchedCities") ? JSON.parse(Cookie.get("lastSearchedCities")) : [];
+console.log(lastSearchedCities.length);
 
 window.addEventListener("DOMContentLoaded", () => {
     document.querySelector("form").addEventListener("submit", e => {
-
+        lastSearchedCities = Cookie.get("lastSearchedCities") ? JSON.parse(Cookie.get("lastSearchedCities")) : [];
         let cityName = document.querySelector(".weather__input").value;
 
-        if (lastSearchedCities == undefined) {
-            Cookie.set("lastSearchedCities", cityName, 365);
-        }
-        else if (lastSearchedCities != undefined) {
-            let citiesArray = lastSearchedCities;
-            if (citiesArray.length == 4) {
-                //DOM displays the last 4 searched cities
-                citiesArray.shift();
-                document.querySelector(".weather__cities").children[0].remove();
+        /*
+        When we try to check weather for the first time, we have to create array from single string in order to keep data in local storage 
+        */
+        if (lastSearchedCities.length == 0) {
+            let cityArray = cityName.split(" ");
+            Cookie.set("lastSearchedCities", JSON.stringify(cityArray), 365);
+            console.log('saved in cookie');
+        } else {
+            lastSearchedCities = lastSearchedCities;
+            lastSearchedCities.push(cityName);
+
+            console.log(lastSearchedCities);
+            Cookie.set("lastSearchedCities", JSON.stringify(lastSearchedCities), 365);
+            //we will render only 4 cities in our list
+            if (lastSearchedCities.length == 5) {
+                lastSearchedCities.shift();
+                Cookie.set("lastSearchedCities", JSON.stringify(lastSearchedCities), 365);
+                document.querySelector(".weather__cities").children[0].remove(); //remove last button in cities list
             }
-            citiesArray.push(cityName);
-            Cookie.set("lastSearchedCities", citiesArray, 365);
         }
 
         const btn = document.createElement('button');
@@ -30,10 +37,9 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     /*
-    Let's render previously searched cities, if there are :D
+    Let's render previously searched cities, if there are
     */
-
-    if (lastSearchedCities != undefined) {
+    if (lastSearchedCities.length != 0) {
         for (const currentCity of lastSearchedCities) {
             const btn = document.createElement('button');
             btn.setAttribute('class', 'weather__city btn btn-white');
@@ -45,7 +51,6 @@ window.addEventListener("DOMContentLoaded", () => {
     /*
     User clicks a button with city name
     */
-
     document.querySelectorAll('button').forEach(el => {
         el.addEventListener("click", event => {
             const cityName = event.target.innerText;
